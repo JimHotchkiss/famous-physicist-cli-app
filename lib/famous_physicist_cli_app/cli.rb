5affa39bf29a40c8da5bfb8194d770d7bf2d6f94
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-07-05 15:50:04 +0000

    Working on scrapping loop.  Now able to access name by nation.

--- lib/famous_physicist_cli_app/cli.rb
+++ lib/famous_physicist_cli_app/cli.rb
@@ -2,13 +2,22 @@ require 'pry'
 class CLI
 
   def call
+    physicists_array = Array.new
     doc = Scraper.new.get_page
+    doc_focus = doc.css('div.tile')
+binding.pry
     doc.css('div.tile').css('span').each do |element|
       nationality = element.text
       if nationality.include?("American")
-        puts "#{element.text}"
+        physicists_array.push(element.text) #This works  but I want scientist
+        physicists_array.each do |nation|
+          puts "#{nation}"
+        end
+      elsif nationality.include?("British")
+        physicists_array.push(element.text)
+#Try pushing the sorted physicists into an array and return the array
       end
-binding.pry
+#binding.pry
     end
     puts "********** Welcome to Famous Physicists! **********"
     sleep(1.5)
