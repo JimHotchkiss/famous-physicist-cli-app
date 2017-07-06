13fefd93bd62f1e40e851ec7311a4000669a4545
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-07-06 14:26:07 +0000

    Took Step back; stubbed out cli.

--- lib/famous_physicist_cli_app/cli.rb
+++ lib/famous_physicist_cli_app/cli.rb
@@ -1,16 +1,44 @@
 require 'pry'
-class CLI
+class FamousPhysicistCliApp::CLI
 
   def call
-
     puts "
 
-     ********** The 20 Most Famous Physicists! **********"
-    sleep(1.5)
+     ********** The 20 Most Famous Physicists! **********
 
-    new = Scraper.new
-    binding.pry
+         "
+
+     list
+     sleep(0.50)
+     user_pick
+     sleep(0.50)
+     details
+     sleep(0.50)
+     another_pick
+     sleep(0.5)
+     finish
 
   end
+
+  def list
+    puts 'Names of physicist!'
+  end
+
+  def user_pick
+    puts 'Pick a physicist'
+  end
+
+  def details
+    puts 'Details'
+  end
+
+  def another_pick
+    puts 'Would you like another pick?'
+  end
+
+  def finish
+    puts 'Thanks for visiting.'
+  end
+
 
 end
--- lib/famous_physicist_cli_app/scraper.rb
+++ lib/famous_physicist_cli_app/scraper.rb
@@ -1,27 +1,27 @@
-require 'pry'
-class Scraper
+#require 'pry'
+#class Scraper
 
-  def get_page
-    Nokogiri::HTML(open("http://famousphysicists.org/"))
-  end
+#  def get_page
+#    Nokogiri::HTML(open("http://famousphysicists.org/"))
+#  end
 
-  def list_names
+#  def list_names
 
-    count = 0
+#    count = 0
 
-    physicists_array = Array.new
-    names = get_page.css('a').css('strong') #this returns an array of names
+#    physicists_array = Array.new
+#    names = get_page.css('a').css('strong') #this returns an array of names
 
 
-    names.each_with_index do |name|
-    physicists_array.push(name.text)
-   end
+#    names.each_with_index do |name|
+#    physicists_array.push(name.text)
+#   end
 
-    physicists_array.each do |each_physicists|
-    puts "#{count+=1}. #{each_physicists}"
-    sleep(0.25)
-    end
+#    physicists_array.each do |each_physicists|
+#    puts "#{count+=1}. #{each_physicists}"
+#    sleep(0.25)
+#    end
 
-  end
+#  end
 
-end
+#end
