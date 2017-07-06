969871839f44ba243bc2d50b05f0635a9ed62c8b
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-07-06 11:57:48 +0000

    Moved #scraper from cli to scraper; getting it to run.

--- lib/famous_physicist_cli_app/cli.rb
+++ lib/famous_physicist_cli_app/cli.rb
@@ -3,20 +3,13 @@ class CLI
 
   def call
 
+    count = 0
+
     puts "********** Welcome to Famous Physicists! **********"
     sleep(1.5)
     puts "What country would you like"
     user_input = gets.strip
 
-    physicists_array = Array.new
-    names = Scraper.new.get_page.css('a').css('strong') #this returns an array of names
-
-
-    names.each do |name|
-      physicists_array.push(name.text)
-    end
-    physicists_array # This returns an array of the physicists!!!!
-binding.pry
-  end
+    Scraper.new.get_page.list_names
 
 end
--- lib/famous_physicist_cli_app/scraper.rb
+++ lib/famous_physicist_cli_app/scraper.rb
@@ -5,4 +5,21 @@ class Scraper
     doc = Nokogiri::HTML(open("http://famousphysicists.org/"))
   end
 
+  def list_names
+
+    count = 0
+    physicists_array = Array.new
+
+    names.each_with_index do |name|
+    physicists_array.push(name.text)
+    end
+
+
+    physicists_array.each do |each_physicists|
+    puts "#{count+=1}. #{each_physicists}"
+    sleep(0.25)
+    end
+
+  end
+
 end
