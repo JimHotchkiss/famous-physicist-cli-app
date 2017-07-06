ac8b51dc9eb1cfabd44939b2c2441f342fb1c2f0
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-07-06 11:38:02 +0000

    Changed websites, refactored the scraper, got to return an array.

--- lib/famous_physicist_cli_app/cli.rb
+++ lib/famous_physicist_cli_app/cli.rb
@@ -2,27 +2,21 @@ require 'pry'
 class CLI
 
   def call
-    physicists_array = Array.new
-    doc = Scraper.new.get_page
-    doc_focus = doc.css('div.tile')
-binding.pry
-    doc.css('div.tile').css('span').each do |element|
-      nationality = element.text
-      if nationality.include?("American")
-        physicists_array.push(element.text) #This works  but I want scientist
-        physicists_array.each do |nation|
-          puts "#{nation}"
-        end
-      elsif nationality.include?("British")
-        physicists_array.push(element.text)
-#Try pushing the sorted physicists into an array and return the array
-      end
-#binding.pry
-    end
+
     puts "********** Welcome to Famous Physicists! **********"
     sleep(1.5)
     puts "What country would you like"
     user_input = gets.strip
+
+    physicists_array = Array.new
+    names = Scraper.new.get_page.css('a').css('strong') #this returns an array of names
+
+
+    names.each do |name|
+      physicists_array.push(name.text)
+    end
+    physicists_array # This returns an array of the physicists!!!!
+binding.pry
   end
 
 end
--- lib/famous_physicist_cli_app/scraper.rb
+++ lib/famous_physicist_cli_app/scraper.rb
@@ -2,14 +2,7 @@ require 'pry'
 class Scraper
 
   def get_page
-    doc = Nokogiri::HTML(open("http://www.thefamouspeople.com/physicists.php"))
-  end
-
-  def scrape_page_index
-    self.get_page.css("div.btn btn-primary btn-sm btn-block btn-block-margin").each do |index|
-      physicists_name = index.text
-      binding.pry
-    end
+    doc = Nokogiri::HTML(open("http://famousphysicists.org/"))
   end
 
 end
