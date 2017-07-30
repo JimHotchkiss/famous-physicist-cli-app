a5ab73a274f2692072d72ca7c2e28552664a843c
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-07-30 22:53:09 +0000

    Refactored .new_from_url to eliminate need for if-statement.

--- lib/famous_physicist_cli_app/scraper.rb
+++ lib/famous_physicist_cli_app/scraper.rb
@@ -7,23 +7,13 @@ class FamousPhysicistCliApp::Scraper
   def self.new_from_url
     self.new.profile_url.each_with_index do |url, i|
       doc = Nokogiri::HTML(open(url))
-       if i == 5 || i == 10 || i == 13 || i == 17
-         FamousPhysicistCliApp::Physicists.new(
-         doc.css('div.page-wrapper').css('div#content').css('h2').css('.post-title').text.strip, #name
-         doc.css('div.post-page-content').css('p').text, #profile
-         doc.css('table.basicinfo').css('tr td')[3].text, #famous_for
-         doc.css('table.basicinfo').css('tr td')[0].text, #birth
-         doc.css('table.basicinfo').css('tr td')[1].text #death
-         )
-        else
           FamousPhysicistCliApp::Physicists.new(
-          doc.css('div.hfeed').css('h1').text.strip,
-          doc.css('div.entry').css('p').text,
-          doc.css('table.basicinfo').css('tr')[3].text.strip,
-          doc.css('table.basicinfo').css('tr')[0].text.split(' ')[1..-1].join(' '),
-          doc.css('table.basicinfo').css('tr')[1].text.split(' ')[1..-1].join(' ')
+          doc.css('div.hfeed').css('h1').text.strip, #name
+          doc.css('div.entry').css('p').text, #profile
+          doc.css('table.basicinfo').css('tr')[3].text.strip, #famous_for
+          doc.css('table.basicinfo').css('tr')[0].text.split(' ')[1..-1].join(' '), #birth
+          doc.css('table.basicinfo').css('tr')[1].text.split(' ')[1..-1].join(' ') #death
           )
-        end
       end
     end
 
